251622fdeab82f9284c6f09f16393fc29edc0f5f
shohjahonfr <yoldashevshohjahon18@gmail.com>
2024-02-25 17:22:34 +0500

    router dom ornatildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import MainLayout from './Layouts/MainLayout'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<MainLayout/>}>
+        <Route index element={<Home/>} />
+      </Route>
+    )
+  )
   return (
-    <div>
-     
-    </div>
+    <RouterProvider router={router} />
+  
   )
 }
 
