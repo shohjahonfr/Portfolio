4a2a8b0e736daa247128c5dc0de2c76131c90ad5
shohjahonfr <yoldashevshohjahon18@gmail.com>
2024-02-28 09:18:18 +0500

    Rasmlar Qoyildi

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
 const MainLayout = () => {
   return (
-    <div className='flex flex-col bg-black  min-h-screen'>
+    <div className='flex flex-col   min-h-screen'>
     <Header />
     <main className=' w-full max-w-[1480px] mx-auto px-5 grow font-bold text-2xl'>
       <Outlet />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,48 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-
+import Telegarm from "../img/telegram.svg"
+import Hub from "../img/github.svg"
+import Day from "../img/day.png"
 const Header = () => {
-  return (
-    <div className='w-full max-w-[1580px] mx-auto py-5   font-bold text-2xl '>
-        <div className=' flex justify-between items-center px-10'>
-            
-            <div>
-            <NavLink className=' text-white' to="/"><h1>Yo'ldashev</h1></NavLink>
+    return (
+        <div className='w-full max-w-[1580px] mx-auto py-5   font-bold text-2xl '>
+            <div className=' flex justify-between items-center px-5'>
+
+                <div>
+                    <NavLink className=' text-slate-300' to="/"><h1>Yo'ldashev</h1></NavLink>
+                </div>
+                <nav className=''>
+                    <div className='w-full '>
+                        <div className=" flex justify-between items-center">
+                            <ul className=' text-slate-300 text-xl space-x-10 mr-10 px-8 font-semibold flex justify-between items-center '>
+
+                                <li className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute after:h-1  after:w-0 after:transition-all after:duration-500 hover:text-blue-600 hover:after:w-full">
+                                    <NavLink className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute   after:w-0 after:transition-all after:duration-500 hover:text-blue-600 hover:after:w-full" to="/">Uy</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute  after:w-0 after:transition-all after:duration-500 hover:text-blue-600 hover:after:w-full" to="/Abaut">Haqida</NavLink>
+                                </li>
+                                <li className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute  after:w-0 after:transition-all after:duration-500 hover:text-blue-600 hover:after:w-full">
+                                    <NavLink className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute  after:w-0 after:transition-all after:duration-500 hover:text-blue-600 hover:after:w-full" to="/Tehnalogy">Tehnalogialar</NavLink>
+                                </li>
+                                <li className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute  after:w-0 after:transition-all after:duration-500 hover:text-blue-600 hover:after:w-full">
+                                    <NavLink className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute  after:w-0 after:transition-all after:duration-500 hover:text-blue-600 hover:after:w-full" to="/Work">Loyihalar</NavLink>
+                                </li>
+                            </ul>
+                            <div className='space-x-5 flex'>
+                                <a href="https://t.me/Yoldashev_FR" target="_blank" ><img src={Telegarm} className='w-8 h-10' alt="Telegram" /></a>
+                                <a href="https://github.com/shohjahonfr"  target="_blank"><img src={Hub} className='w-8 h-10' alt="" /></a>
+                                <button><img src={Day} className='w-8 h-8' alt="" /></button>
+                            </div>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </nav>
             </div>
-            <nav  className=''>
-            <div className='w-full  '>
-          <div className="lg:block hidden">
-            <ul className=' text-slate-300 text-xl rounded-md  px-8 font-semibold flex justify-between items-center gap-x-10'>
-              
-            <li className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute after:h-1 after:bg-blue-600 after:w-0 after:transition-all after:duration-300 hover:text-blue-600 hover:after:w-full">
-                <NavLink className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute after:h-1 after:bg-blue-600 after:w-0 after:transition-all after:duration-300 hover:text-blue-600 hover:after:w-full"to="/">Uy</NavLink>
-              </li>
-              <li>
-                <NavLink className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute after:h-1 after:bg-blue-600 after:w-0 after:transition-all after:duration-300 hover:text-blue-600 hover:after:w-full"to="/Abaut">Haqida</NavLink>
-              </li>
-              <li className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute after:h-1 after:bg-blue-600 after:w-0 after:transition-all after:duration-300 hover:text-blue-600 hover:after:w-full">
-                <NavLink className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute after:h-1 after:bg-blue-600 after:w-0 after:transition-all after:duration-300 hover:text-blue-600 hover:after:w-full"to="/Tehnalogy">Tehnalogialar</NavLink>
-              </li>
-              <li className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute after:h-1 after:bg-blue-600 after:w-0 after:transition-all after:duration-300 hover:text-blue-600 hover:after:w-full">
-                <NavLink className="relative after:left-0 after:bottom-0 after:translate-y-full after:absolute after:h-1 after:bg-blue-600 after:w-0 after:transition-all after:duration-300 hover:text-blue-600 hover:after:w-full"to="/Work">Loyihalar</NavLink>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <a href=""></a>
-          </div>
-        </div> 
-            </nav>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
