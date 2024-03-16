21f3913a9ab4ebe87c468fb76fb102d8f530382d
shohjahonfr <yoldashevshohjahon18@gmail.com>
2024-03-16 14:42:54 +0500

    rasm

--- src/pages/Tehnalogy.jsx
+++ src/pages/Tehnalogy.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import Hub from "../img/github.svg"
+import Html from "../img/html.svg"
+import Css from "../img/css.svg"
+import Boots from "../img/bootstrap.svg"
+import Tailwind from "../img/tailwindcss.svg"
+import Js from "../img/js.svg"
+import react from "../img/react.svg"
+import redux from "../img/redux.svg"
+import redux from "../img/redux.svg"
+import git5 from "../img/git5.png"
 const Tehnalogy = () => {
   return (
-    <div>Tehnalogy</div>
+    <div>
+      <div className='w-20 h-20'>
+         <img src={Html}alt="html" />
+         <img src={Css}alt="css" />
+         <img src={Boots}alt="botstrap" />
+         <img src={Tailwind}alt="tailwindcss" />
+         <img src={Js}alt="Js" />
+         <img src={react}alt="react" />
+         <img src={redux}alt="Redux" />
+         <img src=""alt="git5" />
+      </div>
+    </div>
   )
 }
 
